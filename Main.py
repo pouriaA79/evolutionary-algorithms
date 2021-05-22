e9f2959b1b6dd78c444279ca0ee10f4ccc79a55d
pouriaA79 <80884677+pouriaA79@users.noreply.github.com>
2021-05-22 20:50:00 +0430

    evaluation added

--- Main.py
+++ Main.py
@@ -17,7 +17,7 @@ class Game:
         self.current_level_index += 1
         self.current_level_len = len(self.levels[self.current_level_index])
 
-    def get_score(self, actions):
+    def get_score(self, actions, flag_eval):
         # Get an action sequence and determine the steps taken/score
         # Return a tuple, the first one indicates if these actions result in victory
         # and the second one shows the steps taken
@@ -60,7 +60,11 @@ class Game:
                 flag_end = False
                 current_score = 0
         score.append(current_score)
-        if flag_end:
+        if flag_eval and flag_end:
+            return max(score) + 5, True
+        elif flag_eval and not flag_end:
+            return max(score), False
+        elif flag_end and not flag_eval:
             return max(score) + 5
         else:
             return max(score)
@@ -78,15 +82,16 @@ def read_file(name):
     return len(str), str
 
 
-def population(len):
+def population(len, chromosome_num):
     # chromosomes = [[random.randint(0,2) for i in range(len)] for j in range(200)]
-    chromosome = [[np.random.choice(np.arange(0, 3), p=[0.5, 0.25, 0.25]) for i in range(len)] for j in range(200)]
+    chromosome = [[np.random.choice(np.arange(0, 3), p=[0.5, 0.25, 0.25]) for i in range(len)] for j in
+                  range(chromosome_num)]
     return chromosome
 
 
-def convert_str(chro):
+def convert_str(chro, num, length):
     chromosomes_s = []
-    for i in range(200):
+    for i in range(num):
         s = ""
         for j in range(length):
             s += str(chro[i][j])
@@ -94,15 +99,26 @@ def convert_str(chro):
     return chromosomes_s
 
 
-def fitness(chromosome, moves, num_chromosome):
+def fitness(chromosome, moves, num_chromosome, eval):
     g = Game([moves])
     g.load_next_level()
     scr = []
+    final_result = {}
     for i in range(num_chromosome):
         # print(chromosome[i])
-        scr.append(g.get_score(chromosome[i]))
+        if eval:
+            s, flag_end = g.get_score(chromosome[i], True)
+            final_result.setdefault(i, []).append(s)
+            final_result.setdefault(i, []).append(flag_end)
+            final_result.setdefault(i, []).append(chromosome[i])
+        else:
+            scr.append(g.get_score(chromosome[i], False))
+
         # print(scr[i])
-    return scr
+    if not eval:
+        return scr
+    else:
+        return final_result
 
 
 def selection(scr, chromosome):
@@ -124,16 +140,16 @@ def selection(scr, chromosome):
     return select_score, select_chromosome_score
 
 
-def crossover(scores, chromosome):
+def crossover(scores, chromosome, num):
     cross = chromosome.copy()
-    print(cross)
+    # print(cross)
     # print(cross.get(0)[1])
     new_chromosome = []
 
-    for i in range(20):
+    for i in range(int(num / 10)):
         new_chromosome.append(cross.get(i)[1])
 
-    for i in range(len(scores) - 20):
+    for i in range(len(scores) - int(num / 20)):
         p1 = random.randint(0, 99)
         p2 = random.randint(0, 99)
         parent1 = chromosome.get(p1)[1]
@@ -189,19 +205,49 @@ def mutation(chromosome):
                         s.append(chromosome[i][j])
             chromosome[i] = str.join(s)
 
-
-
     return chromosome
 
 
-if __name__ == "__main__":
-    file_name = "level3.txt"
-    length, moves = read_file(file_name)
-    chromosomes1 = population(length)
-    chromosomes_str = convert_str(chromosomes1)
-    scores = fitness(chromosomes_str, moves, 200)
-    selected_score, selected_chromosomes = selection(scores, chromosomes_str)
-    new_choromosomes = crossover(selected_score, selected_chromosomes)
-    mutation_chromosome = mutation(new_choromosomes)
+def evaluation(chromosome, moves, num_chromosome):
+    final = fitness(chromosome, moves, num_chromosome, True)
+    sort = sorted(final.items(), key=lambda x: x[1], reverse=True)
 
-    
+    return sort
+
+
+def genetic(file_name, chromosome_num, n_iters):
+    length, moves = read_file(file_name)
+    chromosomes1 = population(length, chromosome_num)
+    chromosomes_str = convert_str(chromosomes1, chromosome_num, length)
+    scores_level = []
+    for i in range(n_iters):
+        print(i)
+        if i == 0:
+            scores = fitness(chromosomes_str, moves, chromosomes_num, False)
+        else:
+            scores = fitness(chromosomes_str, moves, chromosomes_num, False)
+        scores_level.append(sum(scores) / len(scores))
+        if abs(scores_level[i] - scores_level[i - 1] )< 0.00001 and i>0:
+            print(scores_level[i], scores_level[i - 1],scores_level[i] - scores_level[i - 1] )
+            break
+        selected_score, selected_chromosomes = selection(scores, chromosomes_str)
+        new_choromosomes = crossover(selected_score, selected_chromosomes, chromosome_num)
+        chromosomes_str = mutation(new_choromosomes)
+    sort_answer = evaluation(chromosomes_str, moves, chromosome_num)
+
+    for i in range(len(sort_answer)):
+        if sort_answer[i][1][1]:
+            return sort_answer[i][1][0], sort_answer[i][1][1], sort_answer[i][1][2]
+
+    return sort_answer[0][1][0], sort_answer[0][1][1], sort_answer[0][1][2]
+
+
+if __name__ == "__main__":
+    file_names = "level10.txt"
+    chromosomes_num = 200
+    n_iter = 500
+    score, end, chromosome = genetic(file_names, chromosomes_num, n_iter)
+    if end :
+        print("pass the level with " + str(score)+" score and  with "+chromosome+ " moves.")
+    else:
+        print("cant pass the level best score : " + str(score) + "  with " + chromosome + " moves.")
